ca9a4950e3907b4fc9622bee53ea2a9f9057aea6
chakresh <chakresh@acis.io>
2021-02-06 14:15:02 +0530

    Updates

---
+++ precache-manifest.d95b5fa85aa94de77b42268d9a14e9ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee010b0f05fcdd455d74a28449d0629",
+    "url": "/nameit/index.html"
+  },
+  {
+    "revision": "c793f15c6e79f4efc89e",
+    "url": "/nameit/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aae99117f17bbe1635c3",
+    "url": "/nameit/static/css/main.6b4d9e66.chunk.css"
+  },
+  {
+    "revision": "c793f15c6e79f4efc89e",
+    "url": "/nameit/static/js/2.23e104d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nameit/static/js/2.23e104d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae99117f17bbe1635c3",
+    "url": "/nameit/static/js/main.1f13c858.chunk.js"
+  },
+  {
+    "revision": "0f6348e1176099d873b9",
+    "url": "/nameit/static/js/runtime-main.3e91652e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nameit/static/media/logo.5d5d9eef.svg"
+  }
+]);
